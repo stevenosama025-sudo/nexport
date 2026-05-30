5ee2cfa72f0f81d4bc742d28abc0f4daa8c7bbc2
zeroman <stevenosama025@gmail.com>
2026-05-30 22:25:34 +0300

    Update lookup.sh

--- nexport/modules/lookup.sh
+++ nexport/modules/lookup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Copyright (c) 2026 Steven Osama (zeroman). All rights reserved.
+# Lead Developer: Steven Osama | GitHub: @stevenosama025-sudo
+
 lookup_port() {
   local query="${1,,}"
   local raw="$1"
